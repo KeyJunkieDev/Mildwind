4b42764556ca24dc284055f4b4acbe2e18eee737
Ratchet Miles <ratchetmiles@gmail.com>
2015-09-15 01:00:21 -0400

    Um... C++?
    I don’t know what I’m doing…

---
+++ Mildwind.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    /* version */
+    std::string version = "Alpha 0.4.0.4 (Windows Edition)\n";
+    bool experimental_version = false;
+    std::cout << version;
+};
