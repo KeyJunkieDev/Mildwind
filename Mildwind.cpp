ec87fc7edd380ea919ab04f1a88ee60caebcc3cb
Ratchet Miles <ratchetmiles@gmail.com>
2015-11-19 04:08:08 -0500

    ???

--- Mildwind.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    /* version */
-    std::string version = "Alpha 0.4.0.4 (Windows Edition)\n";
-    bool experimental_version = false;
-    std::cout << version;
-};
